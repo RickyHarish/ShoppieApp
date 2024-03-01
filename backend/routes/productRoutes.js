3f5670f971fe20eee43f4c73160b39f2284cac2f
RickyHarish <rickyharish30@gmail.com>
2024-03-01 13:25:28 +0530

    Shoppe App

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,37 +1,13 @@
 import express  from "express";
-import Product from "../models/productModel.js";
-import asyncHandler from 'express-async-handler'
+import { getProducts, getProductById, deleteProduct, updateProduct, createProduct } from "../controllers/productController.js";
+import { admin, protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router()
 
-//@desc Fetch all products 
-//@route GET /api/products
-//access Public
 
-router.get('/', asyncHandler(async(req, res)=>{
-    const products = await Product.find({})
-    res.json(products)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+.put(protect, admin, updateProduct)
 
-}))
-
-
-
-//@desc Fetch single product 
-//@route GET /api/products/:id
-//access Public
-
-
-router.get('/:id', asyncHandler(async (req, res)=>{
-    const product = await Product.findById(req.params.id)
-
-    if(product){
-        res.json(product)
-
-    }else{
-        res.status(404)
-        throw new Error("Product Not Found")
-    }
-    
-}))
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,32 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res)=>{
     res.send("API is Running...")
 })
 
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+
 app.use('/api/products', productRoutes)
+app.get('/api/config/paypal', (req, res)=>(res.send(process.env.PAYPAL_CLIENT_ID)))
 
 app.use(notFound)
 
 app.use(errorHandler)
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold))
